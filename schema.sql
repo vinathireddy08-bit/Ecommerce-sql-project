977faf2f06c649967880f9e2e51116a0a1c9ebd6
Vinathi Kaithi <vinathireddy08@gmail.com>
2026-04-23 19:40:11 +0530

    Fix SQL syntax and table definitions in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,32 +1,36 @@
-create DATABASES ecommerce_db;
-use ecommerce_db;
-create table customers(
-    customer_id int primary key,
-    name varchar(50),
-    email varchar(100),
-    city varchar(50),
-    signup_date date
+
+CREATE DATABASE ecommerce_db;
+USE ecommerce_db;
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    city VARCHAR(50),
+    signup_date DATE
 );
-create  table products(
-    product_id int primary key,
-    product_name varchar(100),
-    category varchar(50),
-    price decimal(10,2)
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
 );
-create table orders(
-    order_id int primary key,
-    customer_id int,
-    order_date date,
-    total amount decimal(10,2)
-foeign key(customer_id) references customer(customer_id)
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10,2),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
-create table order_items(
-    order_item_id int primary key,
-    order_id int,
-    product_id int,
-    quanty int,
-    price decimal(10,2),
-    foreign key(order_id) references orders(order_id),
-    foreign key(product_id) references prodects(product_id),
-    foreign key(customer_id) references customer(customer_id)
+
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10,2),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
